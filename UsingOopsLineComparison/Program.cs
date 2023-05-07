48cacf690f527997e852c06e6b856d13ac8a7442
Rahulgautama <rahul8874950727@gmail.com>
2023-05-07 21:35:31 +0530

    Welcome To LineCoparison program using Ooops Concept

---
+++ UsingOopsLineComparison/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UsingOopsLineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LineComparison Program Using Oops Concept");
+        }
+    }
+}
